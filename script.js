9298904c69c9f7bbfa373f7d911d661d54731e87
aquaryasss <nicoletereine@gmail.com>
2025-07-13 21:37:17 +0800

    Added small comments

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-function showSheldonIntro() {
+function showSheldonIntro() { // show the player will go againt sheldon (computer)
     return new Promise(resolve => {
+        // show text
         const sheldonText = document.createElement('div');
         sheldonText.id = 'sheldon-text';
         sheldonText.textContent = 'You will go against Sheldon.';
@@ -11,6 +12,8 @@ function showSheldonIntro() {
                 sheldonText.remove();
             }, 500);
         }, 4000);
+
+        // show sheldon icon
         const sheldonIconDiv = document.createElement('div');
         sheldonIconDiv.id = 'sheldon-div';
         sheldonIconDiv.classList.add('fade-in');
@@ -36,79 +39,86 @@ function showSheldonIntro() {
     });
 }
 
-function showSelectScreen() {
+function showSelectScreen() { // show character options for player
+    // show text
     const characterText = document.createElement('div');
     characterText.id = 'character-text';
     characterText.textContent = 'Select a character.';
     characterText.classList.add('fade-in');
     characterText.classList.add('hide-character-screen');
     screen.appendChild(characterText);
-}
 
-function showCharacters() {
+    // create main container for three characters
     const charactersContainer = document.createElement('div');
     charactersContainer.id = 'characters-container';
     screen.appendChild(charactersContainer);
+
+    // create container for each character
     const leonardIconDiv = document.createElement('div');
     const howardIconDiv = document.createElement('div');
     const rajIconDiv = document.createElement('div');
+
+    // add id and classes for each character
     leonardIconDiv.id = 'leonard-div';
     leonardIconDiv.classList.add('character-div');
     leonardIconDiv.classList.add('fade-in');
     leonardIconDiv.classList.add('hide-character-screen');
+
     howardIconDiv.id = 'howard-div';
     howardIconDiv.classList.add('character-div');
     howardIconDiv.classList.add('fade-in');
     howardIconDiv.classList.add('hide-character-screen');
+
     rajIconDiv.id = 'raj-div';
     rajIconDiv.classList.add('character-div');
     rajIconDiv.classList.add('fade-in');
     rajIconDiv.classList.add('hide-character-screen');
+
+    // add individual containers to the main container
     charactersContainer.appendChild(leonardIconDiv);
     charactersContainer.appendChild(howardIconDiv);
     charactersContainer.appendChild(rajIconDiv);
+
+    // add images of characters
     const leonardIcon = document.createElement('img');
     const howardIcon = document.createElement('img');
     const rajIcon = document.createElement('img');
+
     leonardIcon.src = 'images/leonardicon.png';
     leonardIcon.classList.add('character');
     leonardIcon.classList.add('fade-in');
     leonardIconDiv.appendChild(leonardIcon);
+
     howardIcon.src = 'images/howardicon.png';
     howardIcon.classList.add('character');
     howardIcon.classList.add('fade-in');
     howardIconDiv.appendChild(howardIcon);
+
     rajIcon.src = 'images/rajicon.png';
     rajIcon.classList.add('character');
     rajIcon.classList.add('fade-in');
     rajIconDiv.appendChild(rajIcon);
 }
 
-function showLeonardChosen(){
-        characterText.remove();
-        charactersContainer.remove();
-        const leonardChosen = document.createElement('div');
-        leonardChosen.id = 'leonard-chosen-text';
-        leonardChosen.textContent = "I'm invoking the 'I Win' clause of the Roommate Agreement.";
-        screen.appendChild(leonardChosen);
-}
-
-
 const screen = document.querySelector('#screen');
 const startButton = document.querySelector('#start-button');
 const hideMainMenu = document.querySelectorAll('.hide-main-menu');
-startButton.addEventListener('click', async () => {
+startButton.addEventListener('click', async () => { // if start button is cliked, start the game
+    // hide main menu 
     hideMainMenu.forEach(element =>{
         element.classList.add('hidden');
     });
+
     await showSheldonIntro();
     showSelectScreen();
-    showCharacters();
+
+    // action when either of the characters are chosen by the player
     const leonardIconDiv = document.querySelector('#leonard-div');
     const howardIconDiv = document.querySelector('#howard-div');
     const rajIconDiv = document.querySelector('#raj-div');
     const characterText = document.querySelector('#character-text');
     const charactersContainer = document.querySelector('#characters-container');
+
     leonardIconDiv.addEventListener('click', () => {
         characterText.remove();
         charactersContainer.remove();
@@ -124,6 +134,7 @@ startButton.addEventListener('click', async () => {
             }, 500);
         }, 5000);
     });
+
     howardIconDiv.addEventListener('click', () => {
         characterText.remove();
         charactersContainer.remove();
@@ -139,6 +150,7 @@ startButton.addEventListener('click', async () => {
             }, 500);
         }, 5000);
     });
+
     rajIconDiv.addEventListener('click', () => {
         characterText.remove();
         charactersContainer.remove();
