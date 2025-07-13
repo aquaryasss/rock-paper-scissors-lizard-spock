6a4c9f254ee907accba1ba9e58825cfcf32c63bd
aquaryasss <nicoletereine@gmail.com>
2025-07-13 22:31:24 +0800

    Refactored character selection to store player choice

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+const characters = {
+    leonard: {
+        name: 'Leonard',
+        iconSrc: 'images/leonardicon.png',
+        quote: "I'm invoking the 'I Win' clause of the Roommate Agreement."
+    },
+
+    howard: {
+        name: 'howard',
+        iconSrc: 'images/howardicon.png',
+        quote: "Time to show Sheldon what a Master's degree can do."
+    },
+
+    raj: {
+        name: 'Raj',
+        iconSrc: 'images/rajicon.png',
+        quote: "Let's do this! For Cinnamon."
+    }
+};
+
+let playerCharacter = null;
+
 function showSheldonIntro() { // show the player will go againt sheldon (computer)
     return new Promise(resolve => {
         // show text
@@ -53,51 +75,41 @@ function showSelectScreen() { // show character options for player
     charactersContainer.id = 'characters-container';
     screen.appendChild(charactersContainer);
 
-    // create container for each character
-    const leonardIconDiv = document.createElement('div');
-    const howardIconDiv = document.createElement('div');
-    const rajIconDiv = document.createElement('div');
+    // create the characters
+    for(const element in characters){ // loop through each character in the above database object
+        const characterData = characters[element];
+        const characterDiv = document.createElement('div');
+        characterDiv.classList.add('character-div', 'fade-in');
+        characterDiv.dataset.character = element;
+        const characterIcon = document.createElement('img');
+        characterIcon.src = characterData.iconSrc;
+        characterIcon.classList.add('character');
+        characterDiv.appendChild(characterIcon);
+        charactersContainer.appendChild(characterDiv);
+        characterDiv.addEventListener('click', handleCharacterSelection);
+    }
+}
 
-    // add id and classes for each character
-    leonardIconDiv.id = 'leonard-div';
-    leonardIconDiv.classList.add('character-div');
-    leonardIconDiv.classList.add('fade-in');
-    leonardIconDiv.classList.add('hide-character-screen');
+function handleCharacterSelection(event) {
+    const chosenCharacter = event.currentTarget.dataset.character;
+    playerCharacter = characters[chosenCharacter]; // store player's chosen character
 
-    howardIconDiv.id = 'howard-div';
-    howardIconDiv.classList.add('character-div');
-    howardIconDiv.classList.add('fade-in');
-    howardIconDiv.classList.add('hide-character-screen');
+    // remove select screen
+    document.querySelector('#character-text').remove();
+    document.querySelector('#characters-container').remove();
 
-    rajIconDiv.id = 'raj-div';
-    rajIconDiv.classList.add('character-div');
-    rajIconDiv.classList.add('fade-in');
-    rajIconDiv.classList.add('hide-character-screen');
-
-    // add individual containers to the main container
-    charactersContainer.appendChild(leonardIconDiv);
-    charactersContainer.appendChild(howardIconDiv);
-    charactersContainer.appendChild(rajIconDiv);
-
-    // add images of characters
-    const leonardIcon = document.createElement('img');
-    const howardIcon = document.createElement('img');
-    const rajIcon = document.createElement('img');
-
-    leonardIcon.src = 'images/leonardicon.png';
-    leonardIcon.classList.add('character');
-    leonardIcon.classList.add('fade-in');
-    leonardIconDiv.appendChild(leonardIcon);
-
-    howardIcon.src = 'images/howardicon.png';
-    howardIcon.classList.add('character');
-    howardIcon.classList.add('fade-in');
-    howardIconDiv.appendChild(howardIcon);
-
-    rajIcon.src = 'images/rajicon.png';
-    rajIcon.classList.add('character');
-    rajIcon.classList.add('fade-in');
-    rajIconDiv.appendChild(rajIcon);
+    // display chosen character's text
+    const chosenText = document.createElement('div');
+    chosenText.id = `${chosenCharacter}-chosen-text`;
+    chosenText.textContent = playerCharacter.quote;
+    chosenText.classList.add('fade-in');
+    screen.appendChild(chosenText);
+    setTimeout(() => {
+        chosenText.classList.add('fade-out');
+        setTimeout(() => {
+            chosenText.remove();
+        }, 500);
+    }, 5000);
 }
 
 const screen = document.querySelector('#screen');
@@ -111,61 +123,5 @@ startButton.addEventListener('click', async () => { // if start button is cliked
 
     await showSheldonIntro();
     showSelectScreen();
-
-    // action when either of the characters are chosen by the player
-    const leonardIconDiv = document.querySelector('#leonard-div');
-    const howardIconDiv = document.querySelector('#howard-div');
-    const rajIconDiv = document.querySelector('#raj-div');
-    const characterText = document.querySelector('#character-text');
-    const charactersContainer = document.querySelector('#characters-container');
-
-    leonardIconDiv.addEventListener('click', () => {
-        characterText.remove();
-        charactersContainer.remove();
-        const leonardChosen = document.createElement('div');
-        leonardChosen.id = 'leonard-chosen-text';
-        leonardChosen.textContent = "I'm invoking the 'I Win' clause of the Roommate Agreement.";
-        leonardChosen.classList.add('fade-in');
-        screen.appendChild(leonardChosen);
-        setTimeout(() => {
-            leonardChosen.classList.add('fade-out');
-            setTimeout(() => {
-                leonardChosen.remove();
-            }, 500);
-        }, 5000);
-    });
-
-    howardIconDiv.addEventListener('click', () => {
-        characterText.remove();
-        charactersContainer.remove();
-        const howardChosen = document.createElement('div');
-        howardChosen.id = 'howard-chosen-text';
-        howardChosen.textContent = "Time to show Sheldon what a Master's degree can do.";
-        howardChosen.classList.add('fade-in');
-        screen.appendChild(howardChosen);
-        setTimeout(() => {
-            howardChosen.classList.add('fade-out');
-            setTimeout(() => {
-                howardChosen.remove();
-            }, 500);
-        }, 5000);
-    });
-
-    rajIconDiv.addEventListener('click', () => {
-        characterText.remove();
-        charactersContainer.remove();
-        const rajChosen = document.createElement('div');
-        rajChosen.id = 'raj-chosen-text';
-        rajChosen.textContent = "Let's do this! For Cinnamon!";
-        rajChosen.classList.add('fade-in');
-        screen.appendChild(rajChosen);
-        setTimeout(() => {
-            rajChosen.classList.add('fade-out');
-            setTimeout(() => {
-                rajChosen.remove();
-            }, 500);
-        }, 5000);
-    });
-
 }); 
 
